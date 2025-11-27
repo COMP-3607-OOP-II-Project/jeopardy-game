d5a3db6961aa877d7a0fcea3b446dd877fea4d69
Ana Duque-Hosein <anaduquehosein@gmail.com>
2025-11-27 00:44:52 -0400

    Comments

--- src/main/java/com/uwi/Player.java
+++ src/main/java/com/uwi/Player.java
@@ -1,5 +1,8 @@
 package com.uwi;
-
+/*
+  Represents a player in the game.
+  Each player has a unique identifier and a score that can be updated.
+ */
 public class Player {
     private String id;
     private int score;
@@ -8,20 +11,20 @@ public class Player {
         this.id = id;
         this.score = 0;
     }
-
+   //Returns the player's unique identifier
     public String getId() {
         return id;
     }
 
-
+   //Returns the current score of the player
     public int getScore() {
         return score;
     }
-
+  //Updates the player's score by adding the specified points.
     public void updateScore(int points) {
         score += points;
     }
-
+  //Returns a string representation of the player
     @Override
     public String toString() {
         return id + " (Score: " + score + ")";
